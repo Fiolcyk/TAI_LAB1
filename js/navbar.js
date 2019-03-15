70c18b78e4dc7f39ff1f5d503a1d9c4c55df2754
Fiolcyk <Fiolcyk@gmail.com>
2019-03-15 15:23:36 +0100

    restructure

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ let nav = '<nav class="navbar navbar-light bg-light mojaZmienna">'+
 '<ul class="nav nav-tabs mojaZmienna">'+
         '<a class="nav-link" href="index.html">Główna</a>'+
         '<a class="nav-link" href="index.html">Blog</a>'+ 
-        '<a class="nav-link" href="index.html">Quiz</a>'+  
+        '<a class="nav-link" href="Quiz.html">Quiz</a>'+  
         '<a class="nav-link" href="contact.html">Kontakt</a>'+   
 '</ul>'+
 '</nav>';
